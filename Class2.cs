4bd6f7fbcfb314d865580ffc0c04fe7601093213
SaiPrabhu611 <41958569+SaiPrabhu611@users.noreply.github.com>
2019-01-08 23:36:20 +0530

    sir i just tried to make a json file for the shots and hits once check

---
+++ Class2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace
+{
+    class Class2
+    {
+        private List<hits> hands;
+
+        internal List<hits> Hands
+        {
+            get
+            {
+                return hands;
+            }
+
+            set
+            {
+                hands = value;
+            }
+        }
+    }
+}
---
+++ hits.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace
+{
+    class hits
+    {
+        private List<GameObject> bullets = new List<GameObject>();
+        private int shots;
+        private int hits;
+
+        [SerializeField]
+        private Text hitsText;
+        [SerializeField]
+        private Text shotsText;
+        [SerializeField]
+        private GameObject menu;
+        [SerializeField]
+        private GameObject[] targets;
+        private bool isPaused = false;
+
+        public List<GameObject> Bullets
+        {
+            get
+            {
+                return bullets;
+            }
+
+            set
+            {
+                bullets = value;
+            }
+        }
+
+        public int Shots
+        {
+            get
+            {
+                return shots;
+            }
+
+            set
+            {
+                shots = value;
+            }
+        }
+
+        public int Hits
+        {
+            get
+            {
+                return hits;
+            }
+
+            set
+            {
+                hits = value;
+            }
+        }
+
+        void start()
+        {
+            Cursor.lockState = UnityEngine.CursorLockMode.Confined;
+            Hits = PlayerPrefs.GetInt("hits", 0);
+            Shots = PlayerPrefs.GetInt("shots", 0);
+            shotsText.text = Shots.ToString();
+            hitsText.text = Hits.ToString();
+        }
+    }
+}
