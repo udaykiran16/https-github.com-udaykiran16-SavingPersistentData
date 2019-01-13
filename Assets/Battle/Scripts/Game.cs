4126b94ef2417d517200c2384448b8aa1388ca61
SaiPrabhu611 <41958569+SaiPrabhu611@users.noreply.github.com>
2019-01-13 20:44:29 +0530

    check the change

--- Assets/Battle/Scripts/Game.cs
+++ Assets/Battle/Scripts/Game.cs
@@ -12,15 +12,7 @@ public class Game : MonoBehaviour
 {
     public List<GameObject> bullets = new List<GameObject>();
 
-
-
-
-    public int hits;
-    public int shots;
-
-
-
-
+    public Save save;
     [SerializeField]
     private Text hitsText;
     [SerializeField]
@@ -36,20 +28,22 @@ public class Game : MonoBehaviour
         Pause();
     }
 
-   
 
+    public string dataPath;
     
     void Start()
     {
+        dataPath = Path.Combine(Application.dataPath, "items.txt");
         Cursor.lockState = CursorLockMode.Confined;
-        hits = PlayerPrefs.GetInt("hits", 0);
-        shots = PlayerPrefs.GetInt("shots", 0);
-        shotsText.text = shots.ToString();
-        hitsText.text = hits.ToString();
+       // save.hits =LoadCharacter(dataPath);
+        //  save.shots = Loadcharacter(dataPath);
+       
+        shotsText.text = save.shots.ToString();
+        hitsText.text = save.hits.ToString();
+     
+     }
 
-        SaveGame();
-
-    }
+  
 
     public void Pause()
     {
@@ -85,107 +79,47 @@ public class Game : MonoBehaviour
                 Pause();
             }
         }
+        SaveCharacter(save.hits, dataPath);
+        SaveCharacter(save.shots, dataPath);
     }
 
-    public void SaveGame()
+    static void SaveCharacter(int a, string path)
     {
-        // 1
-        Save save = CreateSaveGameObject();
+        string jsonString = JsonUtility.ToJson(a);
 
-        // 2
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/StreamingAssets/item.json");
-        bf.Serialize(file, save);
-        file.Close();
-
-        // 3
-        hits = 0;
-        shots = 0;
-        shotsText.text = "Shots: " + shots;
-        hitsText.text = "Hits: " + hits;
-
-
-        Debug.Log("Game Saved");
-    }
-
-
-    private Save CreateSaveGameObject()
-    {
-        Save save = new Save();
-        int i = 0;
-        foreach (GameObject targetGameObject in targets)
+        using (StreamWriter streamWriter = File.CreateText(path))
         {
-            Target target = targetGameObject.GetComponent<Target>();
-            if (target.activeRobot != null)
-            {
-                save.livingTargetPositions.Add(target.position);
-                save.livingTargetsTypes.Add((int)target.activeRobot.GetComponent<Robot>().type);
-                i++;
-            }
+            streamWriter.Write(jsonString);
         }
-
-        save.hits = hits;
-        save.shots = shots;
-
-        return save;
     }
+
+    static Save LoadCharacter(string path)
+    {
+        using (StreamReader streamReader = File.OpenText(path))
+        {
+            string jsonString = streamReader.ReadToEnd();
+            return JsonUtility.FromJson<Save>(jsonString);
+        }
+    }
+   
+
+
+
 
     public void AddShot()
     {
-        shots++;
-        shotsText.text = "Shots: " + shots;
+        save.shots++;
+        shotsText.text = "Shots: " + save.shots;
     }
 
     public void AddHit()
     {
-        hits++;
-        hitsText.text = "Hits: " + hits;
+        save.hits++;
+        hitsText.text = "Hits: " + save.hits;
     }
 
-    public void LoadGame()
-    {
-        // 1
-        if (File.Exists(Application.persistentDataPath + "/StreamingAssets/items.json   "))
-        {
 
-            // 2
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/StreamingAssests/items.json", FileMode.Open);
-            Save save = (Save)bf.Deserialize(file);
-            file.Close();
-
-            // 3
-            for (int i = 0; i < save.livingTargetPositions.Count; i++)
-            {
-                int position = save.livingTargetPositions[i];
-                Target target = targets[position].GetComponent<Target>();
-                target.ActivateRobot((RobotTypes)save.livingTargetsTypes[i]);
-                target.GetComponent<Target>().ResetDeathTimer();
-            }
-
-            // 4
-            shotsText.text = "Shots: " + save.shots;
-            hitsText.text = "Hits: " + save.hits;
-            shots = save.shots;
-            hits = save.hits;
-
-            Debug.Log("Game Loaded");
-
-            Unpause();
-        }
-        else
-        {
-            Debug.Log("No game saved!");
-        }
-    }
-
-    public void SaveAsJSON()
-    {
-        Save save = CreateSaveGameObject();
-        string json = JsonUtility.ToJson(save);
-
-        Debug.Log("Saving as JSON: " + json);
-    }
+  
 }
 
 
